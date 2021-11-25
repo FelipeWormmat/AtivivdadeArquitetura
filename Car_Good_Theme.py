0b7236b114c16725fd64dfab09343d44365d74ea
Felipe <felipe.wormmat@gmail.com>
2021-11-25 15:09:28 -0300

    Init

---
+++ Car_Good_Theme.py
@@ -0,0 +1,46 @@
+import string
+import random
+
+class Vehicle:
+    id = str,
+    model = str,
+    catalogue_price = int,
+    is_electric = bool,
+    license_plate = str,
+    tax_percentage = int
+    
+    def __init__ (self, model, catalogue_price, is_electric):
+        self.model = model
+        self.catalogue_price = catalogue_price
+        self.is_electric = is_electric
+        self.id = CarRegistry.generate_vehicle_id(12)
+        self.license_plate = CarRegistry.generate_vehicle_license(self.id)
+        self.tax_percentage = TaxPercentage.tax_percentage(self)
+
+class CarRegistry:
+    def generate_vehicle_id(length):
+        return ''.join(random.choices(string.ascii_uppercase, k=length))
+
+    def generate_vehicle_license(id):
+        first_section = id[:2]
+        second_section = ''.join(random.choices(string.digits, k=2))
+        third_section = ''.join(random.choices(string.ascii_uppercase, k=2))
+        return f"{first_section}-{second_section}-{third_section}"
+
+class TaxPercentage:
+    def tax_percentage(vehicle):
+        if(vehicle.is_electric):
+            return 0.5
+        else:
+            return 0.2
+
+class Application:
+    def register_vehicle(self, brand: string, catalogue_price: int, is_electric: bool):
+        car = Vehicle(brand, catalogue_price, is_electric)
+        print(f'Marca: {car.model}')
+        print(f'ID: {car.id}')
+        print(f'Placa: {car.license_plate}')
+        print(f'Imposto a ser pago: {car.tax_percentage * car.catalogue_price}')
+
+app = Application()
+app.register_vehicle('Honda Civic LX',127900, True)
